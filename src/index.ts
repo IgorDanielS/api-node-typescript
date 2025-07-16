54d75a7d845993c6ef2e884b06883c1b9f81c063
IgorDanielS <igordaniel1903@gmail.com>
2025-07-16 17:12:10 -0300

    feat: initial config

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { server } from "./server/server";
+
+server.listen(3001, () => {
+  console.log("");
+  console.log(
+    "==================================================================================="
+  );
+  console.log("");
+  console.log("");
+  console.log(
+    "                        Servidor rodando na porta 3001                             "
+  );
+  console.log("");
+  console.log("");
+  console.log(
+    "==================================================================================="
+  );
+});
